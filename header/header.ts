35c5d13022f836c95a536c284cfb1638c90b6c05
Yassir <atertay@gmail.com>
2024-08-22 16:32:02 +0100

    updating mode

--- header/header.ts
+++ header/header.ts
@@ -5,13 +5,28 @@ const darkMode = () => {
   document.body.classList.toggle('dark-theme');
     sunIcon.style.display = 'block'
     moonIcon.style.display = 'none'
+    const isDarkMode = document.body.classList.contains('dark-theme');
+    localStorage.setItem("dark", (<any>isDarkMode));
 }
 
 const lightMode = () => {
   document.body.classList.toggle('dark-theme');
     sunIcon.style.display = 'none'
     moonIcon.style.display = 'block'
+    const isLightMode = document.body.classList.contains('dark-theme');
+    localStorage.setItem("dark", (<any>isLightMode));
+}
+
+const darkmode = localStorage.getItem("dark");
+if(darkmode === 'true'){
+  document.body.classList.add('dark-theme');
+}
+
+const lighmode = localStorage.getItem("mode");
+if(lighmode === 'true'){
+  document.body.classList.add('dark-theme');
 }
 
 sunIcon?.addEventListener("click", lightMode);
 moonIcon?.addEventListener("click", darkMode);
+
